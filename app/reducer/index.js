d0cb478424c5e017fd9ada748cc57cced0617184
abody amer <abdullha.eshaq94@gmail.com>
2018-03-02 21:46:37 +0800

    adding cookies

--- app/reducer/index.js
+++ app/reducer/index.js
@@ -1,4 +1,5 @@
 import {ADD_NOTE,DELETE_NOTE, EDIT_NOTE  } from '../action'
+import {bake_cookie, read_cookie} from 'sfcookies'
 
 const addNote = (action) =>{
 
@@ -36,15 +37,19 @@ const editNote = (state=[], action)=>{
 const notes = (state=[] , action) => {
 
     let notes = null
+    state = read_cookie('notes')
     switch(action.type){
         case ADD_NOTE: 
         notes = [ addNote(action), ...state]
+        bake_cookie('notes' , notes)
         return notes
         case DELETE_NOTE:
         notes = removeNote(state, action.id)
+        bake_cookie('notes' , notes)
         return notes
         case EDIT_NOTE:
         notes = editNote(state, action)
+        bake_cookie('notes' , notes)
         return notes
         default:
         return state
